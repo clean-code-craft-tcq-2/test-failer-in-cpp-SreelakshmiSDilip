3b8d2bfddf2abbbbfb943b43f707fb6f58fc5b74
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-21 21:12:42 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -7,7 +7,7 @@ int alertFailureCount = 0;
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
+    AlertStatus returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
