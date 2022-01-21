f7004ed0842f7ae4fedb7b5a71963c093c1579f6
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-21 20:01:21 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -25,7 +25,11 @@ void alertInCelcius(float farenheit) {
 
 int main() {
     alertInCelcius(400.5);
+    assert(alertFailureCount== 1);
     alertInCelcius(303.6);
+    assert(alertFailureCount== 1);
+    alertInCelcius(505.2);
+    assert(alertFailureCount== 2);
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
