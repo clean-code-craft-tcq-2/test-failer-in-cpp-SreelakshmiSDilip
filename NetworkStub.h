060db874731307443618480ba103c3fa97ae9f59
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-21 20:21:36 +0530

    Update NetworkStub.h

--- NetworkStub.h
+++ NetworkStub.h
@@ -1,3 +1,9 @@
 #include <iostream>
 
-int networkAlertStub(float celcius);
+int networkAlertStub(float celcius) {
+    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
+    return 200;
+}
