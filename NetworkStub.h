7f332a14fc2d2105f4fe1b1c794a6e4cbf212716
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-21 21:00:49 +0530

    Update NetworkStub.h

--- NetworkStub.h
+++ NetworkStub.h
@@ -1,9 +1,18 @@
 #include <iostream>
 
-int networkAlertStub(float celcius) {
+const int c_AlertThresholdTempCel = 200 ;
+struct AlertStatus
+{
+    bool isNetworkAlertStubCalled;
+    int  isTempAlertSuccess;
+}
+
+AlertStruct networkAlertStub(float celcius) {
+   
     std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+    AlertStatus alertStatusForCurrenttemp = {true , 200};
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
-    return 200;
+    return alertStatusForCurrenttemp;
 }
