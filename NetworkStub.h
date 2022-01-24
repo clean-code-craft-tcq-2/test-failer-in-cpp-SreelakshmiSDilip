b3ac85ef4c255e5b8b376463c1c1705379237455
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-24 21:46:39 +0530

    Update NetworkStub.h

--- NetworkStub.h
+++ NetworkStub.h
@@ -7,20 +7,6 @@ enum TESTENV
    Simulation = 1
    } testParameter;
 
-int networkAlert(float celcius , TESTENV testParameter)
-{
-   int l_returnCode = 0
-   if(testParameter == TESTENV::Real)
-   {
-      // add actual network communication code here
-   }
-   else
-   {
-      l_returnCode = networkAlertStub(float celcius);
-   }
-   return l_returnCode;
-}
-
 int  networkAlertStub(float celcius) {
    
     std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
@@ -36,3 +22,18 @@ int  networkAlertStub(float celcius) {
     // Return 500 for not-ok
    
 }
+
+int networkAlert(float celcius , TESTENV testParameter)
+{
+   int l_returnCode = 0;
+   if(testParameter == TESTENV::Real)
+   {
+      // add actual network communication code here
+   }
+   else
+   {
+      l_returnCode = networkAlertStub(float celcius);
+   }
+   return l_returnCode;
+}
+
