9018fc917779b4c514202047b4f14beb94de4a17
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-27 17:05:03 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -6,7 +6,6 @@ int alertFailureCount = 0;
 int returnCode = 0;
 
 void alertInCelcius(float farenheit,int* networkAlertFnPtr(float)) {
-    int *ptrToNetworkAlertFn(float); 
     float celcius = (farenheit - 32) * 5 / 9;
     returnCode = *networkAlertFnPtr(celcius);
     if (returnCode != 200) {
