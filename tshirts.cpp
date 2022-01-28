45bb6f2a473305696feedb137f520c3ffd67a4dc
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-28 20:26:01 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -5,11 +5,15 @@ char size(int cms) {
     char sizeName = '\0';
     if(cms < 38) {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if(cms >=  38 && cms < 42) {
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else if(cms >= 42) {
         sizeName = 'L';
-    }
+    }else if(cms >= 42 && cms <48){
+		sizeName = 'XL';
+	}else {
+		sizeName = 'XXL';
+	}
     return sizeName;
 }
 
@@ -17,10 +21,10 @@ int main() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
-    assert(size(38) != 'L'); // size 38 can  be small or medium
-    assert(size(42) != 'S');   //size 42 can be medium or large
+    assert((size(38) == 'S') ||(size(38) == 'M')); // size 38 can  be small or medium
+    assert((size(42) == 'M') ||(size(42) == 'L'));   //size 42 can be medium or large
     assert(size(1000) != 'S');
-    assert (size(1000) != 'M') ;
+    assert(size(1000) != 'M') ;
     assert (size(1000) != 'L'); // size 1000 is not a valid input
     assert(size(0) != 'S');
     assert (size(0) != 'M') ;
