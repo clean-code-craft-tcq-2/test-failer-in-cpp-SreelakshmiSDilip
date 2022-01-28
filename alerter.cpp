32cc0fdeb4fc8e872fa4a38dd05a0e69aa1da087
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-28 19:17:36 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -6,24 +6,29 @@ int alertFailureCount = 0;
 int returnCode = 0;
 
 void alertInCelcius(float farenheit,int (*networkAlertFnPtr)(float)) {
-    float celcius = (farenheit - 32) * 5 / 9;
+    float celcius = convertFaherenheitToCelcius(farenheit);
     returnCode = (*networkAlertFnPtr)(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
+}
+float convertFaherenheitToCelcius(float fahrenheit)
+{
+	  float celcius = (farenheit - 32) * 5 / 9;
+	  return celcius;
 }
 
 int main() {
     
-    alertInCelcius(400.5 ,networkAlertStub );
+    alertInCelcius(400.5 ,networkAlertStubforNotOK );
     assert(alertFailureCount== 1);
-    alertInCelcius(303.6 ,networkAlertStub );
+    alertInCelcius(303.6 ,networkAlertStubforOK );
     assert(alertFailureCount== 1);
-    alertInCelcius(603.6 , networkAlertStub);
+    alertInCelcius(603.6 , networkAlertStubforNotOK);
     assert(alertFailureCount== 2);
     std::cout << "All is well (maybe!)\n";
     return 0;
