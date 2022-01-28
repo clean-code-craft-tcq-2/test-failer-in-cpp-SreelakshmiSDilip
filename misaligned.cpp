a0da9c9623dd641f7beb33194a2393ec872b0f35
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-28 21:53:53 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -24,7 +24,7 @@ int printColorMap()
 {
 
 	int i = 0, j = 0;
-	for(i = 1; i <= 5; i++) {
+	for(i = 0; i < 5; i++) {
 		for(j = 1; j <= 5; j++) {
 			//const char* strCurrentRowEntry[80] = getCombinedPairNumandColorString(i,j);
 			std::cout <<getCombinedPairNumandColorString(i,j)<< std::endl;
