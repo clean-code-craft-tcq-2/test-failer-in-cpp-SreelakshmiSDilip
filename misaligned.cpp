9d29f1841abee161b9c08a4e3e3649f49b439f3c
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-28 22:45:24 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -48,7 +48,7 @@ void testColorCodingReferenceManual(int majorColorId, int minorColorId ,std::str
 int main() {
 	int result = printColorMap();
 	assert(result == 25);
-	testColorCodingReferenceManual(0, 0,"iiiii"); 
+	testColorCodingReferenceManual(0,0,"iiiii"); 
 	/*testColorCodingReferenceManual(1 , 1, "	        2	|White  |Orange");
 	testColorCodingReferenceManual(2 , 2, "	        3	|White  |Green ");
 	testColorCodingReferenceManual(3 , 3, "	        4	|White  |Brown ");
