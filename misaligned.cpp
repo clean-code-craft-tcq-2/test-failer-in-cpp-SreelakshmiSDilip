46c5f6f27a46b47b777bd059d8b0b923977d45ff
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-30 18:30:16 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -38,7 +38,8 @@ void testColorCodingReferenceManual(int majorColorId, int minorColorId ,std::str
 {
 
 	std:: string actualPairNumberandColor = getCombinedPairNumandColorString(majorColorId,minorColorId);
-	assert(actualPairNumberandColor.compare(expectedPairNumberandColor) == 0);
+	//assert(actualPairNumberandColor.compare(expectedPairNumberandColor) == 0);
+	assert(strcmp(expectedPairNumberandColor , actualPairNumberandColor) == 0);
 	std::cout<<std::endl<<expectedPairNumberandColor;
 	std::cout<<std::endl<<actualPairNumberandColor;
 }
