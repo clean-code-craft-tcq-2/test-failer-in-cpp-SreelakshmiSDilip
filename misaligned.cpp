3a5dd563809e7f53c58bfc142e3ed575c107194e
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-30 18:17:28 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -48,7 +48,7 @@ void testColorCodingReferenceManual(int majorColorId, int minorColorId ,std::str
 int main() {
 	//int result = printColorMap();
 	//assert(result == 25);
-        testColorCodingReferenceManual(0 , 0, "	2	|White	|Blue");
+        testColorCodingReferenceManual(0 , 0, "	1	|White	|Blue");
 	testColorCodingReferenceManual(0 , 1, "	2	|White	|Orange");
 	testColorCodingReferenceManual(0 , 2, "	3	|White	|Green");
         testColorCodingReferenceManual(0 , 3, "	4	|White	|Brown");
@@ -58,21 +58,22 @@ int main() {
 	testColorCodingReferenceManual(1 , 2, "	8	|Red	|Green");
         testColorCodingReferenceManual(1 , 3, "	9	|Red	|Brown");
         testColorCodingReferenceManual(1 , 4, "	10	|Red	|Slate");
-	testColorCodingReferenceManual(2,  0, " 11	|Black  |Blue  ");
-	testColorCodingReferenceManual(2,  1, " 12	|Black  |Orange");
-	testColorCodingReferenceManual(2,  2, " 13	|Black  |Green ");
-	testColorCodingReferenceManual(2,  3, " 14	|Black  |Brown ");
-	testColorCodingReferenceManual(2,  4, " 15	|Black  |Slate ");
-	testColorCodingReferenceManual(3,  0, " 16	|Yellow |Blue  ");
-	testColorCodingReferenceManual(3,  1, " 17	|Yellow |Orange");
-	testColorCodingReferenceManual(3,  2, " 18	|Yellow |Green ");
-	testColorCodingReferenceManual(3,  3, " 19	|Yellow |Brown ");
-	testColorCodingReferenceManual(3,  4, " 20	|Yellow |Slate ");
-	testColorCodingReferenceManual(4,  0, " 21	|Violet |Blue  ");
-	testColorCodingReferenceManual(4,  1, " 22	|Violet |Orange");
-	testColorCodingReferenceManual(4,  2, " 23	|Violet |Green ");
-	testColorCodingReferenceManual(4,  3, " 24	|Violet |Brown ");
-	testColorCodingReferenceManual(4,  4, " 25	|Violet |Slate ");
+	testColorCodingReferenceManual(2 , 0, "	11	|Black	|Blue");
+        testColorCodingReferenceManual(2 , 1, "	12	|Black	|Orange");
+	testColorCodingReferenceManual(2 , 2, "	13	|Black	|Green");
+        testColorCodingReferenceManual(2 , 3, "	14	|Black	|Brown");
+        testColorCodingReferenceManual(2 , 4, "	15	|Black	|Slate");
+	testColorCodingReferenceManual(3 , 0, "	16	|Yellow	|Blue");
+        testColorCodingReferenceManual(3 , 1, "	17	|Yellow	|Orange");
+	testColorCodingReferenceManual(3 , 2, "	18	|Yellow	|Green");
+        testColorCodingReferenceManual(3 , 3, "	19	|Yellow	|Brown");
+        testColorCodingReferenceManual(3 , 4, "	20	|Yellow	|Slate");
+       	testColorCodingReferenceManual(4 , 0, "	21	|Violet	|Blue");
+        testColorCodingReferenceManual(4 , 1, "	22	|Violet	|Orange");
+	testColorCodingReferenceManual(4 , 2, "	23	|Violet	|Green");
+        testColorCodingReferenceManual(4 , 3, "	24	|Violet	|Brown");
+        testColorCodingReferenceManual(4 , 4, "	25	|Violet	|Slate");
+
 	std::cout << "All is well (maybe!)\n";
 	return 0;
 }
