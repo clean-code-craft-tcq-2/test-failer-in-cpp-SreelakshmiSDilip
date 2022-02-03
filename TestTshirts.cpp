56fa62f8070e365a8de14807d9e6783b4d235f2d
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-02-03 11:42:49 +0530

    Update TestTshirts.cpp

--- TestTshirts.cpp
+++ TestTshirts.cpp
@@ -3,19 +3,19 @@
 #include"tshirts.cpp"
 
 
-void TestTshirts(char actualSize ,char expectedSize)
+void TestTshirts(int actualSizeIncms ,char expectedSize)
 {
-    assert(actualSize == expectedSize);
+    assert(size(actualSizeIncms) == expectedSize);
 }
 
 int main() {
-    TestTshirts(size(37) , 'S');
-    TestTshirts(size(40) ,'M');
-    TestTshirts(size(43) , 'L');
-    TestTshirts(size(38) , 'M'); 
-    TestTshirts(size(42) , 'L');   
-    TestTshirts(size(50) , 'U');
-    TestTshirts(size(30) , 'U');
+    TestTshirts(37 , 'S');
+    TestTshirts(40 ,'M');
+    TestTshirts(43 , 'L');
+    TestTshirts(38 , 'M'); 
+    TestTshirts(42 , 'L');   
+    TestTshirts(50 , 'U');
+    TestTshirts(30 , 'U');
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
