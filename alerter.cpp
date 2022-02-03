6cb17934fc6aa8ce2386ec341a40d502b9763645
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-02-03 11:12:56 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -24,14 +24,4 @@ void alertInCelcius(float farenheit,int (*networkAlertFnPtr)(float)) {
 }
 
 
-int main() {
-    
-    alertInCelcius(400.5 ,networkAlertStubforNotOK );
-    assert(alertFailureCount== 1);
-    alertInCelcius(303.6 ,networkAlertStubforOK );
-    assert(alertFailureCount== 1);
-    alertInCelcius(603.6 , networkAlertStubforNotOK);
-    assert(alertFailureCount== 2);
-    std::cout << "All is well (maybe!)\n";
-    return 0;
-}
+
