2277d8b618d2b56a2b2c0f16c4ca9201f07240d9
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-02-03 11:13:38 +0530

    Update TestAlerter.cpp

--- TestAlerter.cpp
+++ TestAlerter.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 #include <assert.h>
 #include "AlerterNetworkHandler.h"
+#include "Alerter.cpp"
+
+
+int main() {
+    
+    alertInCelcius(400.5 ,networkAlertStubforNotOK );
+    assert(alertFailureCount== 1);
+    alertInCelcius(303.6 ,networkAlertStubforOK );
+    assert(alertFailureCount== 1);
+    alertInCelcius(603.6 , networkAlertStubforNotOK);
+    assert(alertFailureCount== 2);
+    std::cout << "All is well (maybe!)\n";
+    return 0;
+}
